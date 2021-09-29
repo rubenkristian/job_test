acf2cc55f2be3dd3180b39cff9ce4b2c58f1dbc9
rubenkristian <rubenkristian96@gmail.com>
2021-09-29 21:09:37 +0700

    comment result variable

--- src/generate_maze_one.js
+++ src/generate_maze_one.js
@@ -4,32 +4,31 @@ function generate_maze_one(s) {
 		maze[i] = Array(s);
 	}
 	let k = 0;
-	let result = "";
+	// let result = "";
 	for(let i = 0; i < s; i++) {
-		result = "";
+		// result = "";
 		for(let j = 0; j < s; j++) {
 			if(j === 0 || j === (s-1)) {
-				result += "@";
+				// result += "@";
 				maze[i][j] = "@";
 			} else {
 				if(i % 2 === 0) {
 					if(k % 2 === 0) {
 						maze[i][j] = (j === 1) ? " " : "@";
-						result += (j === 1) ? " " : "@";
+						// result += (j === 1) ? " " : "@";
 					} else {
 						maze[i][j] = (j === (s - 2)) ? " " : "@";
-						result += (j === (s - 2)) ? " " : "@";
+						// result += (j === (s - 2)) ? " " : "@";
 					}
 				} else {
 					maze[i][j] = " ";
-					result += " ";
+					// result += " ";
 				}
 			}
 		}
 		if(i % 2 == 0) {
 			k++;
 		}
-		// console.log(result);
 	}
 	for(let j = 0; j < s; j++) {
 		console.log(maze[j].join(""));
